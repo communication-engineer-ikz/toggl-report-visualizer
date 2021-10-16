804612406a78d5e68a575429c226d0efc7beefc5
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-10-16 14:37:06 +0900

    読みやすいようにインデントを変更

--- google-apps-script/toggl-report-visualizer.js
+++ google-apps-script/toggl-report-visualizer.js
@@ -10,67 +10,66 @@ var Toggl = {
     BASIC_AUTH: '【ご自身のAPIキー】:api_token',
   
     get: function(path){
-      var url = 'https://www.toggl.com/api/v8' + path;
-      var options = {
-        'method' : 'GET',
-        'headers': {"Authorization" : "Basic " + Utilities.base64Encode(this.BASIC_AUTH)}
-      }
-      var response = UrlFetchApp.fetch(url, options);
-      return JSON.parse(response);
+        var url = 'https://www.toggl.com/api/v8' + path;
+        var options = {
+          'method' : 'GET',
+          'headers': {"Authorization" : "Basic " + Utilities.base64Encode(this.BASIC_AUTH)}
+        }
+        var response = UrlFetchApp.fetch(url, options);
+        return JSON.parse(response);
     },
     getTimeEntries: function(){
-      var path = '/time_entries'
-      return this.get(path)
+        var path = '/time_entries'
+        return this.get(path)
     },
     getTimeEntry: function(id) {
-      var path = '/time_entries/' + id
-      return this.get(path);
+        var path = '/time_entries/' + id
+        return this.get(path);
     },
     getProject: function(id) {
-      var path = '/projects/' + id
-      return this.get(path);
+        var path = '/projects/' + id
+        return this.get(path);
     }
-  }
+}
   
-  var Slack = {  
+var Slack = {  
     post: function(message){
-      var url = '【ご自身で作ったAPIのURL】';
-      var options = {
-        'method': 'POST',
-        'contentType': 'application/json',
-        'payload': JSON.stringify({'text': message})
-      }
-      var response = UrlFetchApp.fetch(url, options);
-      return response;
+        var url = '【ご自身で作ったAPIのURL】';
+        var options = {
+          'method': 'POST',
+          'contentType': 'application/json',
+          'payload': JSON.stringify({'text': message})
+        }
+        var response = UrlFetchApp.fetch(url, options);
+        return response;
     },
-  }
+}
   
-  function main() {
+function main() {
     var togglLogSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TogglLog");
     var timeEntries = Toggl.getTimeEntries();
     var lastTimeEntryId = togglLogSheet.getRange(1,1).getValue();
   
     for(var i in timeEntries) {
-      var timeEntry = timeEntries[i];
+        var timeEntry = timeEntries[i];
   
-      if(timeEntry.id <= lastTimeEntryId) {
-        continue;
-      }
-      if(timeEntry.duration < 0) {
-        continue;
-      }
-  
-      lastTimeEntryId = timeEntry.id
-      var projectName = Toggl.getProject(timeEntry.pid).data.name
-      var data = []
-      togglLogSheet.getRange(togglLogSheet.getLastRow() + 1, 1, 1, togglLogSheet.getLastColumn())
-        .setValues([[timeEntry.id, projectName, timeEntry.description, timeEntry.duration, new Date(timeEntry.start), new Date(timeEntry.stop)]]);
-      Slack.post(formatText(projectName, timeEntry))
+        if(timeEntry.id <= lastTimeEntryId) {
+            continue;
+        }
+        if(timeEntry.duration < 0) {
+            continue;
+        }
+    
+        lastTimeEntryId = timeEntry.id
+        var projectName = Toggl.getProject(timeEntry.pid).data.name
+        var data = []
+        togglLogSheet.getRange(togglLogSheet.getLastRow() + 1, 1, 1, togglLogSheet.getLastColumn())
+            .setValues([[timeEntry.id, projectName, timeEntry.description, timeEntry.duration, new Date(timeEntry.start), new Date(timeEntry.stop)]]);
+        Slack.post(formatText(projectName, timeEntry))
     }
     togglLogSheet.getRange(1,1).setValue(lastTimeEntryId);
-  }
+}
   
-  function formatText(projectName, timeEntry) {
+function formatText(projectName, timeEntry) {
     return '【' + projectName + '】 *' + timeEntry.description + '* ：' + Math.round(timeEntry.duration/60/60*100)/100 + '時間'
-  }
-
+}

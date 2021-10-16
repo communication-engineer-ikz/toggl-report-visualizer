9af324bdb0813a13106af58437f10d2894669474
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-10-16 16:21:54 +0900

    不要な行を削除

--- google-apps-script/toggl-report-visualizer.js
+++ google-apps-script/toggl-report-visualizer.js
@@ -3,10 +3,7 @@
     * https://qiita.com/e99h2121/items/18533f39dc3643c1ceaf
  */
 function togglReportVisualizer() {
-    //Toggl の当日のレポートを取得
     const togglReportArray = makeTogglReportArray();
-
-    //GSS へ転記
     postTogglReportVisualizationSheet(togglReportArray);
 
     //GSS のレコードを横棒グラフに整形
@@ -59,45 +56,4 @@ function postTogglReportVisualizationSheet(array) {
     const targetRange = sheet.getRange(lastRow + 1, 1, array[0].length, array.length);
 
     return targetRange.setValues(array);
-}
-  
-var Slack = {  
-    post: function(message){
-        var url = '【ご自身で作ったAPIのURL】';
-        var options = {
-          'method': 'POST',
-          'contentType': 'application/json',
-          'payload': JSON.stringify({'text': message})
-        }
-        var response = UrlFetchApp.fetch(url, options);
-        return response;
-    },
-}
-  
-function main() {
-    var togglLogSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TogglLog");
-    var lastTimeEntryId = togglLogSheet.getRange(1,1).getValue();
-  
-    for(var i in timeEntries) {
-        var timeEntry = timeEntries[i];
-  
-        if(timeEntry.id <= lastTimeEntryId) {
-            continue;
-        }
-        if(timeEntry.duration < 0) {
-            continue;
-        }
-    
-        lastTimeEntryId = timeEntry.id
-        var projectName = Toggl.getProject(timeEntry.pid).data.name
-        var data = []
-        togglLogSheet.getRange(togglLogSheet.getLastRow() + 1, 1, 1, togglLogSheet.getLastColumn())
-            .setValues([[timeEntry.id, projectName, timeEntry.description, timeEntry.duration, new Date(timeEntry.start), new Date(timeEntry.stop)]]);
-        Slack.post(formatText(projectName, timeEntry))
-    }
-    togglLogSheet.getRange(1,1).setValue(lastTimeEntryId);
-}
-  
-function formatText(projectName, timeEntry) {
-    return '【' + projectName + '】 *' + timeEntry.description + '* ：' + Math.round(timeEntry.duration/60/60*100)/100 + '時間'
 }
